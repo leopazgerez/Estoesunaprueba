f26aa0cad62615e39ddd63d1d77908fcc8077171
Leonel Paz Gerez <104148448+leopazgerez@users.noreply.github.com>
2022-07-15 17:37:59 -0300

    Update main.dart

--- otrotestmas/lib/main.dart
+++ otrotestmas/lib/main.dart
@@ -2,16 +2,99 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
+
+/*void main() {
+  //Esta funcion toma al Widget y lo hace la raiz del ArbolWidget
+  //En este ejemplo, el arbol tiene 2 Widgets, 'CENTER' y su
+  //hijo 'CHILD' el texto.\
+  //La estructura fuerza a la raiz del arbol a cubrir la pantalla
+  //con el texto, loque significa que va a estar centrado en 
+  //la pantalla
+  runApp(
+    const Center(
+        child: Text(
+      'Hola querido mundo!!',
+      textDirection: TextDirection.ltr,
+    )),
+  );
+}*/
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, super.key});
+  //Los campos en una sublase Widget son siempre marcados como
+  // 'Final'
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) /*(Constructor de contexto)*/ {
+    return Container(
+        //permite crear un elemento visual rectangular
+        height: 56.0, //en pixeles logicos(altura)
+        padding: const EdgeInsets.symmetric(
+            vertical: 15.0, horizontal: 8.0), //(Relleno)
+        decoration: BoxDecoration(
+            color: Colors.red[600],
+            border: Border.all(
+              color: Colors.white,
+              width: 8,
+            )), //Decoracion
+        //La fila es horizontal con diseno lineal
+        child: Row(
+          children: [
+            const IconButton(
+              icon: Icon(Icons.restore),
+              tooltip: 'Navegation menu',
+              onPressed: null, //nulo deshabilita el boton
+            ),
+            //'Expanded' expande este hijo para llenar el espacio
+            //disponible
+            Expanded(
+              child: title,
+            ),
+            const IconButton(
+              icon: Icon(Icons.agriculture),
+              tooltip: 'Search',
+              onPressed: null,
+            ),
+          ],
+        ));
+  }
+}
+
+class MyScaffold extends StatelessWidget /*(Widget sin estado)*/ {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //'Material' es una pieza conceptual
+    //de papel en donde el UI aparece
+    return Material(
+        //La columna tiene diseno lineal vertical
+        child: Column(children: [
+      MyAppBar(
+        title: Text(
+          ' Example title',
+          style: Theme.of(context).primaryTextTheme.headline6,
+        ),
+      ),
+      const Expanded(
+        child: Center(
+          child: Text('Hola querido Mundo!!'),
+        ),
+      ),
+    ]));
+  }
+}
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        'Hola querido mundo!!',
-        textDirection: TextDirection.ltr,
-      )
+    const MaterialApp(
+      title: 'Mi aplicacion', //usado por el conmutador de tareas del S.O
+      home: SafeArea(
+        child: MyScaffold(),
+      ),
     ),
   );
 }
-
